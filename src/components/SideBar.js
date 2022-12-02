ce13985fe3f70f25796c0c6abbc794b5aaaf5cda
Ian <ianmcconnell376@gmail.com>
2022-12-02 17:36:26 -0500

    Work on sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,38 +6,55 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { Link } from 'react-router-dom';
+import ProjectsIcon from '@mui/icons-material/Business';
+import ReportsIcon from '@mui/icons-material/Folder';
+import ProfileIcon from '@mui/icons-material/Person';
 
-const drawerWidth = 240;
+const drawerWidth = 300;
 
 export default function SideBar() {
   return (
-    <Drawer
+      <Drawer style={{ background: '#667DB9'}}
       sx={{
         width: drawerWidth,
-        flexShrink: 0,
+              flexShrink: 0,
         '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box',
+            width: drawerWidth,
+            boxSizing: 'border-box'
         },
       }}
       variant="permanent"
       anchor="left"
     >
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+          <Divider />
+          <img style={{ width: 100, height: 100 }} src="/logo192.png" alt="Logo" />
+          <List>
+              <ListItem button disablePadding component={Link} to="/ProjectListPage">
+                  <ListItemButton>
+                      <ListItemIcon>
+                          <ProjectsIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Projects" />
+                  </ListItemButton>
+              </ListItem>
+              <ListItem button disablePadding component={Link} to="/ProjectOverviewPage">
+                  <ListItemButton>
+                      <ListItemIcon>
+                          <ReportsIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Reports" />
+                  </ListItemButton>
+              </ListItem>
+              <ListItem style={{ paddingTop: '200%' }} disablePadding>
+                  <ListItemButton>
+                      <ListItemIcon>
+                          <ProfileIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Profile" />
+                  </ListItemButton>
+              </ListItem>
+          </List>
     </Drawer>
   );
 }
